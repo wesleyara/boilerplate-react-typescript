a0b5477fe809cb2dc3c86c5d02bde192e93a9aa5
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-01-09 20:59:20 -0300

    :wrench: Adicionando react router ao index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root"),
 );
